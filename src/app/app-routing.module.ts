03968c70402e991480df900142324ddabddf945e
Maria Alejandra Ortiz <maortizm@localhost.localdomain>
2019-03-04 17:33:59 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: "about", component : AboutComponent},
+  { path: "home", component : HomeComponent},
+  { path: "services", component : ServicesComponent},
+  { path: "gallery", component : GalleryComponent},
+  { path: "testimonials", component : TestimonialsComponent},
+  { path: "contact", component : ContactComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: 
+    [
+      RouterModule.forRoot(routes)
+    ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    GalleryComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
